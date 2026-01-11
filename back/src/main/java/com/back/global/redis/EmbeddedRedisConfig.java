3940337607a81ed0c18feeb0e478fd9f4a7b456b
nekopunch <taiikichan@naver.com>
2026-01-11 09:41:34 +0900

    work

---
+++ back/src/main/java/com/back/global/redis/EmbeddedRedisConfig.java
@@ -0,0 +1,33 @@
+package com.back.global.redis;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import redis.embedded.RedisServer;
+
+import java.io.IOException;
+
+@Profile("!prod")
+@Configuration
+public class EmbeddedRedisConfig {
+
+    @Value("${spring.data.redis.port}")
+    private int redisPort;
+
+    private RedisServer redisServer;
+
+    @PostConstruct
+    public void startRedis() throws IOException {
+        redisServer = new RedisServer(redisPort);
+        redisServer.start();
+    }
+
+    @PreDestroy
+    public void stopRedis() throws IOException {
+        if (redisServer != null) {
+            redisServer.stop();
+        }
+    }
+}
